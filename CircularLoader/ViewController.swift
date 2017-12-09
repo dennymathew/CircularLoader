de10eef1bda6f84593db116bfe3e87fb1f3fb77c
Denny Mathew <denny.mathew@cabotsolutions.com>
2017-12-09 21:45:40 +0530

    Initial Commit

---
+++ CircularLoader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CircularLoader
+//
+//  Created by Denny Mathew on 09/12/17.
+//  Copyright © 2017 Denny Mathew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
